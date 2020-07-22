2a9908fa747b4f71318f578e0f019f3aecbe261e
Michiel Leyman <michiel.leyman@gmail.com>
2020-07-22 20:21:14 +0200

    Get SERVER_URL from environment

--- src/bin/global.ts
+++ src/bin/global.ts
@@ -1,3 +1,3 @@
-exports.SERVER_URL = 'http://localhost:' + process.env.PORT;
+exports.SERVER_URL = process.env.SERVER_URL || `http://localhost:${process.env.PORT || 5000}`;
 
 exports.FRONTEND_URL = 'https://wisebadges.be';
