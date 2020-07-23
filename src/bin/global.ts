2476b4fcfa4ccdef00322843a89bffac7ea9b887
freyavs <freyavanspeybroeck@outlook.com>
2020-07-23 11:29:57 +0200

    assertions pagination

--- src/bin/global.ts
+++ src/bin/global.ts
@@ -1,2 +1,4 @@
 exports.SERVER_URL = process.env.SERVER_URL || `http://localhost:${process.env.PORT || 5000}`;
 exports.FRONTEND_URL = 'https://wisebadges.be';
+exports.DEFAULT_PAGE_LIMIT = 20;
+exports.DEFAULT_PAGE_MAX = 50;
